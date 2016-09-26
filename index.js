b54c052aa0b902f03c81c61e1d8c07dc89b3585c
Andrew <atandiawan@gmail.com>
2016-09-26 15:28:35 +0700

    fixing port

--- index.js
+++ index.js
@@ -2,8 +2,14 @@
 let express = require('express')
 let app = express()
 let routes = require('./routes/users.js')
-app.listen(process.env.NODE_ENV||3000,function(){
-  console.log('listening on 3000')
-})
+app.set('port', (process.env.PORT || 5000));
 
 app.use('/api', routes)
+
+app.get('/', function(){
+  res.send('welcome')
+})
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
